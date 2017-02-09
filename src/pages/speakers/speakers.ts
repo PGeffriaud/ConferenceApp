0c923248e850699fcaaed64ea1283d4d7ed6ac1c
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-02-09 20:03:36 +0100

    Speakers list

--- src/pages/speakers/speakers.ts
+++ src/pages/speakers/speakers.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { Speaker } from '../../types/Speaker'
+import { SpeakerService } from '../../services/speakers.service'
+import { SpeakerDetail } from '../speaker-detail/speaker-detail'
+
 @Component({
   selector: 'page-speakers',
-  templateUrl: 'speakers.html'
+  templateUrl: 'speakers.html',
+  providers: [SpeakerService]
 })
 export class Speakers {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  speakers: Array<Speaker>
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private speakerService: SpeakerService) {
+
+    speakerService.getSpeakers().then(speakers => {
+      this.speakers = speakers;
+    })
+  }
+
+  lookAt(speakerId: string): void {
+    this.navCtrl.push(SpeakerDetail, {id: speakerId})
+  }
 
 
 }
