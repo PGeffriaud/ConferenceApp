f6003e1b21dfcaa4c3b2415a666b5b09c17ce0dc
Pierre Geffriaud <geffriaud.pierre@gmail.com>
2017-02-16 08:52:36 +0100

    Improve About page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Device } from 'ionic-native';
+import { Device, AppVersion, InAppBrowser } from 'ionic-native';
 
 @Component({
   selector: 'page-about',
@@ -9,15 +9,35 @@ import { Device } from 'ionic-native';
 export class About {
 
   info: any
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
 
+    AppVersion.getAppName().then(res => {
+      this.info.appName = res
+    })
+
+    AppVersion.getVersionNumber().then(res => {
+      this.info.appVersion = res
+    })
+
     this.info = {
-      uuid: Device.uuid,
       platform: Device.platform,
       model: Device.model,
       version: Device.version,
       manufacturer: Device.manufacturer
     }
+  }
+
+  open(link: string): void {
+    var url: string
+
+    switch(link) {
+      case 'github': url = 'https://github.com/PGeffriaud/ConferenceApp'; break;
+      case 'linkedin': url = 'https://www.linkedin.com/in/pierre-geffriaud-b0915bb9'; break;
+      case 'mygithub': url = 'https://github.com/PGeffriaud/'; break;
+    }
+    new InAppBrowser(url, '_blank', 'location=no');
+
   }
 
 
